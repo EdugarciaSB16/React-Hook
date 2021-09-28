07dc67ab938318c6b8af1940f7f7e0e86b77cf79
Eduardo García <edugarciasb16@gmail.com>
2021-09-29 01:32:16 +0200

    use Context

---
+++ src/components/09-useContext/LoginScreen.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
+
+const LoginScreen = () => {
+
+    const {setUser} = useContext(UserContext)
+
+    return (
+        <div>
+            <h1>LoginScreen</h1>
+            <hr />
+            <button className="btn btn-primary" onClick={ () => setUser({
+                name: 'Eduardo',
+                email: 'eduardo@gmail.com'
+            })}>
+                Login
+            </button>
+        </div>
+    );
+};
+
+export default LoginScreen;
